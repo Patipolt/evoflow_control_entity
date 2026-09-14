acc43a7d0b111c0bfef81afea58318163f04f547
Patipol Thanuphol <nuprovident@gmail.com>
2026-09-14 09:03:07 +0200

    Added new pump calibration (KPMP10) pump 1-2

--- evoflow/device/evoflow.py
+++ evoflow/device/evoflow.py
@@ -225,7 +225,7 @@ class EvoFlowDevice:
                 if not packet:
                     continue
 
-                if True and t_first_byte is not None:
+                if verbose and t_first_byte is not None:
                     t_end = time.perf_counter()
                     print(tc(
                         f"read_serial latency: first byte after {(t_first_byte - t_start) * 1000:.2f} ms, "
